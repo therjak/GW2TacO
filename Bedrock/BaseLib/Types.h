7c3f81aea45eae00e934d17ba53d9e81d995f32a
therjak <therjak@gmx.net>
2021-06-26 21:22:29 +0200

    clean up Types.h and note the broken typedefs

--- Bedrock/BaseLib/Types.h
+++ Bedrock/BaseLib/Types.h
@@ -2,16 +2,7 @@
 
 #include <cstdint>
 
-//basic type definitions
+//type definitions with broken use
 
-// typedef unsigned char		uint8_t;
-// typedef unsigned short		uint16_t;
-// typedef unsigned int		uint32_t;
-// typedef unsigned __int64	uint64_t;
 typedef char				TS8;
-// typedef short				int16_t;
-// typedef int					int32_t;
-// typedef __int64				int64_t;
-// typedef float				float;
-// typedef double				double;
 typedef unsigned char		TBOOL;
