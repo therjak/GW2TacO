c0885c0adcd08fe3b7506638c4f3c6d204d27b59
therjak <therjak@gmx.net>
2021-12-29 23:10:52 +0100

    made color constexpr

--- src/base/color.h
+++ src/base/color.h
@@ -2,6 +2,16 @@
 
 #include <cstdint>
 
+constexpr float clamp(float l, float v, float r) {
+  if (v < l) {
+    return l;
+  }
+  if (v > r) {
+    return r;
+  }
+  return v;
+}
+
 class CColor {
   uint8_t b = 0, g = 0, r = 0, a = 0;
 
@@ -19,8 +29,13 @@ class CColor {
 
   constexpr CColor(const CColor&) = default;
 
-  static CColor FromFloat(const float _r, const float _g, const float _b,
-                          const float _a);
+  static constexpr CColor FromFloat(const float _r, const float _g,
+                                    const float _b, const float _a) {
+    return CColor(static_cast<uint8_t>(clamp(0, _r, 1) * 255),
+                  static_cast<uint8_t>(clamp(0, _g, 1) * 255),
+                  static_cast<uint8_t>(clamp(0, _b, 1) * 255),
+                  static_cast<uint8_t>(clamp(0, _a, 1) * 255));
+  }
 
   constexpr bool operator==(const CColor& c) const {
     return r == c.r && g == c.g && b == c.b && a == c.a;
@@ -28,16 +43,23 @@ class CColor {
   constexpr bool operator!=(const CColor& c) const {
     return r != c.r || g != c.g || b != c.b || a != c.a;
   }
-  uint32_t argb() const;
+  constexpr uint32_t argb() const {
+    return (a << 24) | (r << 16) | (g << 8) | b;
+  }
 
-  uint8_t& R() noexcept { return r; }
-  uint8_t& G() noexcept { return g; }
-  uint8_t& B() noexcept { return b; }
-  uint8_t& A() noexcept { return a; }
+  constexpr uint8_t& R() noexcept { return r; }
+  constexpr uint8_t& G() noexcept { return g; }
+  constexpr uint8_t& B() noexcept { return b; }
+  constexpr uint8_t& A() noexcept { return a; }
   constexpr uint8_t R() const { return r; }
   constexpr uint8_t G() const { return g; }
   constexpr uint8_t B() const { return b; }
   constexpr uint8_t A() const { return a; }
 };
 
-const CColor Lerp(const CColor a, const CColor b, const float t);
+constexpr CColor Lerp(const CColor a, const CColor b, const float t) {
+  return CColor(static_cast<uint8_t>((b.R() - a.R()) * t + a.R()),
+                static_cast<uint8_t>((b.G() - a.G()) * t + a.G()),
+                static_cast<uint8_t>((b.B() - a.B()) * t + a.B()),
+                static_cast<uint8_t>((b.A() - a.A()) * t + a.A()));
+}
