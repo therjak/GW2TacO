0c5e7e9f8c2cd0e189340392bbe6189061275b86
therjak <therjak@gmx.net>
2021-12-29 22:53:59 +0100

    made rectangle constexpr

--- src/base/rectangle.h
+++ src/base/rectangle.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <cstdint>
 
 #include "src/base/vector.h"
@@ -8,45 +9,128 @@ class CRect {
  public:
   int32_t x1 = 0, y1 = 0, x2 = 0, y2 = 0;
 
-  CRect() = default;
-  CRect(const int32_t a, const int32_t b, const int32_t c, const int32_t d);
-  CRect(const CPoint p1, const CPoint p2);
-  const bool Contains(const int32_t x, const int32_t y) const;
-  const bool Contains(CPoint& p) const;
-  const bool Contains(const CPoint& p) const;
-  const int32_t Width() const;
-  const int32_t Height() const;
-  const int32_t Area() const;
-  const bool Intersects(const CRect& r) const;
+  constexpr CRect() = default;
+  constexpr CRect(const int32_t a, const int32_t b, const int32_t c,
+                  const int32_t d)
+      : x1(a), y1(b), x2(c), y2(d) {}
+  constexpr CRect(const CPoint p1, const CPoint p2)
+      : x1(p1.x), y1(p1.y), x2(p2.x), y2(p2.y) {}
+  constexpr bool Contains(const int32_t x, const int32_t y) const {
+    return x >= x1 && x < x2 && y >= y1 && y < y2;
+  }
+  constexpr bool Contains(const CPoint& p) const { return Contains(p.x, p.y); }
+  constexpr int32_t Width() const { return x2 - x1; }
+  constexpr int32_t Height() const { return y2 - y1; }
+  constexpr int32_t Area() const { return Width() * Height(); }
+  constexpr bool Intersects(const CRect& r) const {
+    return !(x2 <= r.x1 || x1 >= r.x2) && !(y2 <= r.y1 || y1 >= r.y2);
+  }
 
-  CRect operator+(const CRect& a) const;
-  CRect operator-(const CRect& a) const;
-  CRect operator*(const int32_t a) const;
-  CRect& operator+=(const CRect& a);  // inflate by rect
-  CRect operator+(const CPoint& p) const;
-  CRect& operator+=(const CPoint& p);
-  CRect operator-(const CPoint& p) const;
-  CRect& operator-=(const CPoint& p);
-  const bool operator==(const CRect& r) const;
-  const bool operator!=(const CRect& r) const;
-  CRect operator|(const CRect& r) const;
-  CRect& operator|=(const CRect& r);
-  CRect operator&(const CRect& r) const;
-  CRect& operator&=(const CRect& r);
+  constexpr CRect operator+(const CRect& a) const {
+    return CRect(x1 - a.x1, y1 - a.y1, x2 + a.x2, y2 + a.y2);
+  }
+  constexpr CRect operator-(const CRect& a) const {
+    return CRect(x1 + a.x1, y1 + a.y1, x2 - a.x2, y2 - a.y2);
+  }
+  constexpr CRect operator*(const int32_t a) const {
+    return CRect(x1 * a, y1 * a, x2 * a, y2 * a);
+  }
+  // inflate by rect
+  constexpr CRect& operator+=(const CRect& a) {
+    x1 -= a.x1;
+    y1 -= a.y1;
+    x2 += a.x2;
+    y2 += a.y2;
+    return *this;
+  }
 
-  void Move(int32_t x, int32_t y);
-  void Move(const CPoint& p);
-  void MoveTo(int32_t x, int32_t y);
-  void SetSize(int32_t x, int32_t y);
-  void Normalize();
-  CPoint TopLeft() const;
-  CPoint BottomRight() const;
-  CPoint TopRight() const;
-  CPoint BottomLeft() const;
-  CSize Size() const;
-  CPoint Center() const;
-  CRect GetCenterRect(CSize& s);
-  CRect GetIntersection(const CRect& r) const;
+  constexpr CRect operator+(const CPoint& p) const {
+    return CRect(x1 + p.x, y1 + p.y, x2 + p.x, y2 + p.y);
+  }
+  constexpr CRect& operator+=(const CPoint& p) {
+    Move(p);
+    return *this;
+  }
+  constexpr CRect operator-(const CPoint& p) const {
+    return CRect(x1 - p.x, y1 - p.y, x2 - p.x, y2 - p.y);
+  }
+  constexpr CRect& operator-=(const CPoint& p) {
+    Move(-p);
+    return *this;
+  }
+  constexpr bool operator==(const CRect& r) const {
+    return x1 == r.x1 && y1 == r.y1 && x2 == r.x2 && y2 == r.y2;
+  }
+  constexpr bool operator!=(const CRect& r) const {
+    return x1 != r.x1 || y1 != r.y1 || x2 != r.x2 || y2 != r.y2;
+  }
+  constexpr CRect operator|(const CRect& r) const {
+    if (!Intersects(r)) return CRect(1, 1, -1, -1);
+    return GetIntersection(r);
+  }
+  constexpr CRect& operator|=(const CRect& r) {
+    *this = *this | r;
+    return *this;
+  }
+  constexpr CRect operator&(const CRect& r) const {
+    return CRect(x1 < r.x1 ? x1 : r.x1, y1 < r.y1 ? y1 : r.y1,
+                 x2 > r.x2 ? x2 : r.x2, y2 > r.y2 ? y2 : r.y2);
+  }
+  constexpr CRect& operator&=(const CRect& r) {
+    *this = *this & r;
+    return *this;
+  }
+
+  constexpr void Move(int32_t x, int32_t y) {
+    x1 += x;
+    y1 += y;
+    x2 += x;
+    y2 += y;
+  }
+  constexpr void Move(const CPoint& p) { Move(p.x, p.y); }
+  constexpr void MoveTo(int32_t x, int32_t y) {
+    x2 -= (x1 - x);
+    y2 -= (y1 - y);
+    x1 = x;
+    y1 = y;
+  }
+  constexpr void SetSize(int32_t x, int32_t y) {
+    x2 = x1 + x;
+    y2 = y1 + y;
+  }
+  constexpr void Normalize() {
+    int32_t _x1 = x1;
+    int32_t _y1 = y1;
+    int32_t _x2 = x2;
+    int32_t _y2 = y2;
+
+    x1 = _x1 < _x2 ? _x1 : _x2;
+    x2 = _x1 < _x2 ? _x2 : _x1;
+    y1 = _y1 < _y2 ? _y1 : _y2;
+    y2 = _y1 < _y2 ? _y2 : _y1;
+  }
+  constexpr CPoint TopLeft() const { return CPoint(x1, y1); }
+  constexpr CPoint BottomRight() const { return CPoint(x2, y2); }
+  constexpr CPoint TopRight() const { return CPoint(x2, y1); }
+  constexpr CPoint BottomLeft() const { return CPoint(x1, y2); }
+  constexpr CSize Size() const { return CSize(Width(), Height()); }
+  constexpr CPoint Center() const { return CPoint(x1 + x2, y1 + y2) / 2; }
+  constexpr CRect GetCenterRect(CSize& s) {
+    return CRect(Center().x - s.x / 2, Center().y - s.y / 2,
+                 Center().x - s.x / 2 + s.x, Center().y - s.y / 2 + s.y);
+  }
+  constexpr CRect GetIntersection(const CRect& r) const {
+    return CRect(x1 > r.x1 ? x1 : r.x1, y1 > r.y1 ? y1 : r.y1,
+                 x2 < r.x2 ? x2 : r.x2, y2 < r.y2 ? y2 : r.y2);
+  }
 };
 
-bool IntervalIntersection(int32_t a1, int32_t a2, int32_t b1, int32_t b2);
+constexpr bool IntervalIntersection(int32_t a1, int32_t a2, int32_t b1,
+                                    int32_t b2) {
+  int32_t _a1 = std::min(a1, a2);
+  int32_t _a2 = std::max(a1, a2);
+  int32_t _b1 = std::min(b1, b2);
+  int32_t _b2 = std::max(b1, b2);
+
+  return !((a1 < b1 && a2 < b1) || (a1 > b2 && a2 > b2));
+}
