d9cf81674eb6749420d39655093ac378f395c559
therjak <therjak@gmx.net>
2022-03-05 16:38:10 +0100

    replaced a use of sstream with std::format

--- src/base/hasher.cpp
+++ src/base/hasher.cpp
@@ -1,11 +1,14 @@
 #include "src/base/hasher.h"
 
-#include <sstream>
+#include <format>
 
 namespace std {
 std::size_t hash<GUID>::operator()(const GUID& guid) const {
-  std::stringstream ss;
-  ss << std::hex << guid.Data1 << guid.Data2 << guid.Data3 << guid.Data4;
-  return std::hash<std::string>()(ss.str());
+  const auto h = std::format(
+      "{:08x}{:04x}{:04x}{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}",
+      guid.Data1, guid.Data2, guid.Data3, guid.Data4[0], guid.Data4[1],
+      guid.Data4[2], guid.Data4[3], guid.Data4[4], guid.Data4[5], guid.Data4[6],
+      guid.Data4[7]);
+  return std::hash<std::string>()(h);
 }
 }  // namespace std
