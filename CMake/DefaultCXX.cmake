f128abb2e8a1dd8db6ca5907c0d9a534f0786292
therjak <therjak@gmx.net>
2023-12-12 22:34:42 +0100

    clean up CMake files

--- CMake/DefaultCXX.cmake
+++ CMake/DefaultCXX.cmake
@@ -5,8 +5,10 @@ set_config_specific_property("OUTPUT_DIRECTORY" "${CMAKE_SOURCE_DIR}$<$<NOT:$<ST
 if(MSVC)
     create_property_reader("DEFAULT_CXX_EXCEPTION_HANDLING")
     create_property_reader("DEFAULT_CXX_DEBUG_INFORMATION_FORMAT")
+    create_property_reader("DEFAULT_CXX_RUNTIME_LIBRARY")
 
     set_target_properties("${PROPS_TARGET}" PROPERTIES MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL")
     set_config_specific_property("DEFAULT_CXX_EXCEPTION_HANDLING" "/EHsc")
     set_config_specific_property("DEFAULT_CXX_DEBUG_INFORMATION_FORMAT" "/Zi")
+    set_config_specific_property("DEFAULT_CXX_RUNTIME_LIBRARY" "/MD")
 endif()
